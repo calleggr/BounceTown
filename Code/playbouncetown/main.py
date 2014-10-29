2fd1a2cb68cf2c2b500c1bb04c0339f6ee22f0ec
Greg Callegari <gregorycallegari@gregorys-air-2.wlan.rose-hulman.edu>
2014-10-29 19:59:13 -0400

    updated main and models
    adding friends functionality

--- Code/playbouncetown/main.py
+++ Code/playbouncetown/main.py
@@ -18,38 +18,39 @@ import os
 import jinja2
 import webapp2
 
-
-from handlers import friends_handler
-from handlers import main_handller
-from handlers import challenge_handler
-
-from utils import game
+from handlers import game_handlers
+from handlers import main_handlers
 from utils import date_utils
+from utils import game_utils
 
 # Jinja environment instance necessary to use Jinja templates.
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), autoescape=True)
 jinja_env.filters["date_format"] = date_utils.date_format
-jinja_env.filters["game_complete_boolean_format"] = game.game_complete_boolean_format
+jinja_env.filters["game_complete_boolean_format"] = game_utils.game_complete_boolean_format
 
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_env.get_template("templates/home.html")
-        self.response.out.write(template.render())
-        
-class FriendHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_env.get_template("templates/friends.html")
-        self.response.out.write(template.render())
-        
-class ChallengeHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_env.get_template("templates/challenges.html")
-        self.response.out.write(template.render())
+# class MainHandler(webapp2.RequestHandler):
+#     def get(self):
+#         template = jinja_env.get_template("templates/home.html")
+#         self.response.out.write(template.render())
+#         
+# class FriendHandler(webapp2.RequestHandler):
+#     def get(self):
+#         template = jinja_env.get_template("templates/friends.html")
+#         self.response.out.write(template.render())
+#         
+# class ChallengeHandler(webapp2.RequestHandler):
+#     def get(self):
+#         template = jinja_env.get_template("templates/challenges.html")
+#         self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([
-                               ('/', MainHandler),
-                               ('/friends', FriendHandler),
-                               ('/challenges', ChallengeHandler)
-                               ], debug=True)
-                               
+    ('/', main_handlers.HomePage),
+    ('/play', game_handlers.PlayPage),
+    ('/gamesinprogress', game_handlers.GamesInProgressPage),
+    ('/completedgames', game_handlers.CompletedGamesWithFriendsPage),
+
+    ('/setdisplayname', main_handlers.SetDisplayNameAction),
+    ('/newgame', game_handlers.NewGameAction),
+    ('/scoresupdate', game_handlers.ScoresUpdateAction)
+], debug=True)
--- Code/playbouncetown/models.py
+++ Code/playbouncetown/models.py
@@ -1,9 +1,10 @@
 from google.appengine.ext import ndb
 
-
+from endpoints_proto_datastore.ndb.model import EndpointsModel
 
 
 class Player():
+  _message_fields_schema = ("entityKey", "display_name", "past_opponent_emails")
   display_name = ndb.StringProperty()
   past_opponent_emails = ndb.StringProperty(repeated=True)
   hourly_high_score = ndb.StringProperty()
@@ -17,17 +18,17 @@ class Player():
     return self.key.string_id()  # email address
 
 
-class Game():
-  """ BounceTown Game """
-
+class Game(EndpointsModel):
+  """ BounceTown Game. """
+  _message_fields_schema = ("entityKey", "creator_key", "invitee_key", "creator_scores", "invitee_scores",
+                            "last_touch_date_time", "is_complete", "is_solo")
+  creator_key = ndb.KeyProperty(kind=Player)
+  invitee_key = ndb.KeyProperty(kind=Player)
+  creator_scores = ndb.IntegerProperty(repeated=True)
+  invitee_scores = ndb.IntegerProperty(repeated=True)
   last_touch_date_time = ndb.DateTimeProperty(auto_now=True)
-  user_id = ndb.StringProperty()
-  score = ndb.IntegerProperty()
-  
-  
-class Challenge():
-    """ A challenge """
-    is_completed = ndb.BooleanProperty(default=False)
-    challenge_length = ndb.StringProperty()
-    challenger_high_score = ndb.StringProperty()
-    challengee_high_score = ndb.StringProperty()
+  is_complete = ndb.BooleanProperty(default=False)
+  is_solo = ndb.BooleanProperty()
+  # Not in datastore, for message passing only.
+  invitee_email = ndb.StringProperty()
+  new_score = ndb.IntegerProperty()
