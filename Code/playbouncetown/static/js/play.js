f1fd71cb8740645931674c1b6e76f47faf1577cd
Greg Callegari <gregorycallegari@gregorys-air-2.wlan.rose-hulman.edu>
2014-11-03 11:41:47 -0500

    some changes

--- Code/playbouncetown/static/js/play.js
+++ Code/playbouncetown/static/js/play.js
@@ -2,24 +2,56 @@ $(document).ready(function(){
 	
 	console.log("Loaded!");
 	var gameBeingPlayed = true;
+	var enemies = [];
 	var hero = new MainC();
 	var c = document.getElementById("canvas");
 	var ctx = c.getContext("2d");
 	ctx.clearRect(0,0,500,500);
 	var count = 0;
-	var go = true;
+	var numEnemies = 3;
 	var timePassed = setInterval(function() {
-		//console.log("draw");
+		//Clear the canvas
 		ctx.clearRect(0,0,500,500);
+		
+		//Draw the hero
 		ctx.fillStyle=hero.getColor();
 		ctx.beginPath();
-		ctx.arc(hero.getX(),hero.getY(),hero.diameter,0,2*Math.PI,true);
+		ctx.arc(hero.getX(),hero.getY(),hero.radius,0,2*Math.PI,true);
 		ctx.fill();
-		hero.move();
-		//setTimeout(draw(),1000);
 		//End draw hero
+		
+		//Draw enemies
+		for(var i = 0; i< enemies.length;i++){
+			ctx.fillStyle = enemies[i].getColor();
+			ctx.beginPath();
+			ctx.arc(enemies[i].getX(),enemies[i].getY(),enemies[i].radius,0,2*Math.PI,true);
+			ctx.fill();
+			enemies[i].move();
+		}
+		//End draw enemies
+		
+		//Move the hero
+		hero.move();
+		
+		
+		//Check Collisions
+		for(var i = 0;i<enemies.length;i++){
+			var x1 = enemies[i].getX();
+			var y1 = enemies[i].getY();
+			var dx = x1 - hero.getX();
+			var dy = y1 - hero.getY();
+			var dist = Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+			if(dist < (20)){
+				//console.log("Collided!");
+				//console.log(x1 + " , " + hero.getX());
+				//console.log(y1 + " , " + hero.getY());
+			}
+		}
+		//Basic kill function for now.
 		count+=1;
-		//console.log(count);
+		if((count%100 == 0) && (enemies.length < numEnemies)){
+			enemies.push(new Enemy());
+		}
 		if(count == 100000){
 			killInterval();
 		}
@@ -30,7 +62,6 @@ $(document).ready(function(){
 	
 	},10);
 	
-	go = false;
 	console.log("Done with loop");
 	
 	
@@ -42,17 +73,7 @@ $(document).ready(function(){
 	}
 	
 	
-	
-	
-	
-	
-	
-	//Draw things
-	function draw(){
-		
-		
-		
-	}
+
 	
 	
 	
@@ -68,7 +89,7 @@ $(document).ready(function(){
 		this.x = 250;
 		this.y = 250;
 		this.speed = 1;
-		this.diameter = 20;
+		this.radius = 20;
 		this.dx = 1;
 		this.dy = 5;
 		
@@ -96,6 +117,55 @@ $(document).ready(function(){
 		
 		this.getY = function(){
 			return this.y;
+		}
+	}
+	
+	//Define enemy class
+	function Enemy(){
+		this.x = 100;
+		this.y = 100;
+		this.radius = 20;
+		this.dx = 2;
+		this.dy = 3;
+		this.colorCounter = 0;
+		this.color = "#000000";
+		
+		this.move = function(){
+			this.colorCounter++;
+			if(this.colorCounter == 50){
+				this.changeColor();
+				this.colorCounter = 0;
+			}
+			this.x+=this.dx;
+			this.y+=this.dy;
+			if((this.x > 480) || (this.x < 20)){
+				this.dx*=-1;
+			}
+			if((this.y > 480) || (this.y < 20)){
+				this.dy*=-1;
+			}
+		}
+		
+	
+	
+		this.getColor = function(){
+			return this.color;
+		}
+		
+		this.getX = function(){
+			return this.x;
+		}
+		
+		this.getY = function(){
+			return this.y;
+		}
+		this.changeColor = function(){
+			if(this.color == "#000000"){
+				this.color = "#FF0000";
+			}else{
+				this.color = "#000000";
+			}
+			
 		}
 	}
 	
