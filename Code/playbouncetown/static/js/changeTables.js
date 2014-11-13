1d3b015b50623422a22887b24acf6835332cd952
Kevin Trizna <triznakj@rose-hulman.edu>
2014-11-12 23:00:56 -0500

    css changes
    fuck you

---
+++ Code/playbouncetown/static/js/changeTables.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+	
+	var lastTF = document.getElementById('lasttf-table');
+	var lastTen = document.getElementById('lastten-table');
+	var topTF = document.getElementById('toptf-table');
+	var topTen = document.getElementById('topten-table');
+	var hilo = document.getElementById("hilo-table");
+	var lohi = document.getElementById("lohi-table");
+	
+	$(#'lasttf-btn').click(function(){
+		lastTF.toggleClass("hidden");
+		lastTen.toggleClass("hidden");
+		topTF.toggleClass("hidden");
+		topTen.toggleClass("hidden");
+		hilo.toggleClass("hidden");
+		lohi.toggleClass("hidden");
+	});
+	$('#lastten-btn').click(function(){
+		lastTF.toggleClass("hidden");
+		lastTen.toggleClass("hidden");
+		topTF.toggleClass("hidden");
+		topTen.toggleClass("hidden");
+		hilo.toggleClass("hidden");
+		lohi.toggleClass("hidden");
+	});
+	$('#toptf-btn').click(function(){
+		lastTF.toggleClass("hidden");
+		lastTen.toggleClass("hidden");
+		topTF.toggleClass("hidden");
+		topTen.toggleClass("hidden");
+		hilo.toggleClass("hidden");
+		lohi.toggleClass("hidden");
+	});
+	$('#topten-btn').click(function(){
+		lastTF.toggleClass("hidden");
+		lastTen.toggleClass("hidden");
+		topTF.toggleClass("hidden");
+		topTen.toggleClass("hidden");
+		hilo.toggleClass("hidden");
+		lohi.toggleClass("hidden");
+	});
+	$('#hilo-btn').click(function(){
+		lastTF.toggleClass("hidden");
+		lastTen.toggleClass("hidden");
+		topTF.toggleClass("hidden");
+		topTen.toggleClass("hidden");
+		hilo.toggleClass("hidden");
+		lohi.toggleClass("hidden");
+	});
+	$('#lohi-btn').click(function(){
+		lastTF.toggleClass("hidden");
+		lastTen.toggleClass("hidden");
+		topTF.toggleClass("hidden");
+		topTen.toggleClass("hidden");
+		hilo.toggleClass("hidden");
+		lohi.toggleClass("hidden");
+	});
+	
+});
