d306ce3df5bfc84cba8e622b25bb969c1b337479
Kevin Trizna <triznakj@rose-hulman.edu>
2014-11-13 22:58:53 -0500

    Query users
    Users can be queried

--- Code/playbouncetown/main.py
+++ Code/playbouncetown/main.py
@@ -17,14 +17,15 @@
 import os
 import jinja2
 import webapp2
-
+from google.appengine.api import users
 from handlers import main_handlers
 from utils import date_utils
 from handlers import base_handlers
-
+import main
 from models import Player
 from models import Score
-
+from utils import player_utils
+from google.storage.speckle.proto.jdbc_type import NULL
 
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), autoescape=True)
 jinja_env.filters["date_format"] = date_utils.date_format
@@ -81,10 +82,63 @@ class ScoresPage(base_handlers.BasePage):
         values["sorted_max"] = top_scores_max
         
         
-        
-        
+class ScoresPageNewUser(base_handlers.BasePage):
+        #email = self.request.get("new_email")
+        #player = Player.query(Player.email_addr == email)
+        #values = {}
+    def get(self):
+        user = users.get_current_user()
+        if not user:
+            template = main.jinja_env.get_template("templates/home.html")
+            self.response.out.write(template.render({'login_url': users.create_login_url(self.request.referer)}))
+        else:
+            email = self.request.get('new_email')
+            player = player_utils.get_player_from_email(email)
+    
+            values = {'email':email,
+                      'player':player,
+                      'logout_url':users.create_logout_url("/")}
+            self.update_values(player, values)
+            template = main.jinja_env.get_template(self.get_template())
+            self.response.out.write(template.render(values))
+      
+    def get_template(self):
+        return "templates/score.html"
+    
+    def update_values(self, player, values):
+        top_ten = []
+        top_twentyfive = []
+        top_scores = sorted(player.scores)
+        last_ten = []
+        last_twentyfive = []
+        top_scores_max = []
+        for k in range (0,min(len(player.scores), 10)):
+            last_ten.append(player.scores[-1*k])
+        for k in range (0,min(len(player.scores), 25)):
+            last_twentyfive.append(player.scores[-1*k])
+        for k in range (1,min(len(top_scores)-1, 11)):
+            top_ten.append(top_scores[-1*k])
+        for k in range (1,min(len(top_scores)-1, 26)):
+            top_twentyfive.append(top_scores[-1*k])
+            
+        for k in range (0,len(top_scores)-1):
+            top_scores_max.append(top_scores[len(top_scores)-1-k]) 
+              
+        values["top_ten"] = top_ten
+        values["top_twentyfive"] = top_twentyfive
+        values["last_ten"] = last_ten
+        values["last_twentyfive"] = last_twentyfive
+        values["sorted_min"] = top_scores
+        values["sorted_max"] = top_scores_max 
+          
+class UpdatePage(base_handlers.BasePage):
+    def get_template(self):
+        return "templates/updates.html"        
      
-        
+    def update_values(self,player,values):
+        pass
+
+
 
 app = webapp2.WSGIApplication([
     ('/', main_handlers.HomePage),
@@ -92,4 +146,6 @@ app = webapp2.WSGIApplication([
     ('/addScore', AddScore),
     ('/scores', ScoresPage),
     ('/setdisplayname', main_handlers.SetDisplayNameAction),
+    ('/update',UpdatePage),
+    ('/userscores',ScoresPageNewUser)
 ], debug=True)
